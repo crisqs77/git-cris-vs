e9f7baef84a7c852956e2755ee8a227ec0e68d33
Cristhian Quesada <cris.qs.77@gmail.com>
2023-12-17 23:58:16 -0600

    CU02-Empresas(controller,model,servicio,index,_layout,program)

---
+++ ProjectGitVS/Models/Empresa.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectGitVS.Models
+{
+    public class Empresa
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public bool Eliminado { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ ProjectGitVS/Servicios/EmpresaServicio.cs
@@ -0,0 +1,19 @@
+﻿using ProjectGitVS.Models;
+
+namespace ProjectGitVS.Servicios
+{
+    public class EmpresaServicio
+    {
+        public List<Empresa> All()
+        {
+            List<Empresa> listOfCompanies = new List<Empresa>()
+            {
+                new Empresa() { Id = 1, Nombre = "Coca Cola", Eliminado = false, Estado = true },
+                new Empresa() { Id = 2, Nombre = "Nuvel", Eliminado = false, Estado = false },
+                new Empresa() { Id = 3, Nombre = "Yambal", Eliminado = true, Estado = false  },
+                new Empresa() { Id = 4, Nombre = "Samsung", Eliminado = true, Estado = true  }
+            };
+            return listOfCompanies;
+        }
+    }
+}
