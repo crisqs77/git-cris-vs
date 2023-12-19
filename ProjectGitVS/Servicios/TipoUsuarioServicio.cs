af8619a1cff9381cf5d26cc8c9b5bd8d26921365
Cristhian Quesada <cris.qs.77@gmail.com>
2023-12-18 21:09:09 -0600

    CU04-UsuarioTipo(controller,model,servicio,_layout,index,program)

---
+++ ProjectGitVS/Servicios/TipoUsuarioServicio.cs
@@ -0,0 +1,17 @@
+﻿using ProjectGitVS.Models;
+
+namespace ProjectGitVS.Servicios
+{
+    public class TipoUsuarioServicio
+    {
+        public List<TipoUsuario> All()
+        {
+            List<TipoUsuario> listOfTypeUsers = new List<TipoUsuario>()
+            {
+                new TipoUsuario() { Id = 1, Nombre = "Administrador"},
+                new TipoUsuario() { Id = 2, Nombre = "Cliente"},
+            };
+            return listOfTypeUsers;
+        }
+    }
+}
