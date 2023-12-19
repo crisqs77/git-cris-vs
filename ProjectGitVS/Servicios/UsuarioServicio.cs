364821b150e10bad093e51c0ed3eaed26f148fd7
Cristhian Quesada <cris.qs.77@gmail.com>
2023-12-19 14:17:00 -0600

    CU05-ModUsuarios(modelo Usuario, servicio, 2 index)

--- ProjectGitVS/Servicios/UsuarioServicio.cs
+++ ProjectGitVS/Servicios/UsuarioServicio.cs
@@ -8,9 +8,9 @@ namespace ProjectGitVS.Servicios
         {
             List<Usuario> listOfUsers = new List<Usuario>()
             {
-                new Usuario() { Id = 1, Nombre = "John Doe", Edad = 42 },
-                new Usuario() { Id = 2, Nombre = "Jane Doe", Edad = 34 },
-                new Usuario() { Id = 3, Nombre = "Joe Doe", Edad = 7 },
+                new Usuario() { Id = 1, Nombre = "John Doe", Edad = 42, Tipo = new TipoUsuario() { Id = 2, Nombre = "Cliente"} },
+                new Usuario() { Id = 2, Nombre = "Jane Doe", Edad = 34, Tipo = new TipoUsuario() { Id = 1, Nombre = "Administrador"} },
+                new Usuario() { Id = 3, Nombre = "Joe Doe", Edad = 7, Tipo = new TipoUsuario() { Id = 2, Nombre = "Cliente"} },
             };
             return listOfUsers;
         }
